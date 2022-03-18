b36b212474eca8fd481b3999616e454b9e806faf
Haruki Izumihara <izu.0622h@gmail.com>
2022-03-18 15:19:00 +0900

    second commit

---
+++ js/main.js
@@ -0,0 +1,15 @@
+$(function () {
+  $(".js-header-btn").on("click", function () {
+    $(".js-gnav,.js-header-bar").toggleClass("open");
+  });
+});
+
+$(function () {
+  var h = $(window).height();
+  $(".js-loader-bg,.js-loader-fade").height(h).css("display", "block"); //ローディング画像を表示
+});
+$(window).on("load", function () {
+  // 読み込み完了したら実行する
+  $(".js-loader-fade").delay(600).fadeOut(300); // ローディングをフェードアウト
+  $(".js-loader-bg").delay(900).fadeOut(800); // 背景色をフェードアウト
+});
